4e08500316720d035cd325c1b6d4141079575292
Doug Green <doug@dsgreen.com>
2015-06-01 20:43:58 -0400

    Add Bootstrap container classes

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area container-fluid">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
