443650428ddb2732159f2f642787831e0753e735
Doug Green <doug@dsgreen.com>
2015-06-01 20:44:28 -0400

    Add pagination classes

--- comments.php
+++ comments.php
@@ -33,10 +33,10 @@ if ( post_password_required() ) {
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'under-boot' ); ?></h2>
-			<div class="nav-links">
+			<div class="nav-links pager">
 
-				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'under-boot' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'under-boot' ) ); ?></div>
+				<div class="nav-previous previous"><?php previous_comments_link( __( 'Older Comments', 'under-boot' ) ); ?></div>
+				<div class="nav-next next"><?php next_comments_link( __( 'Newer Comments', 'under-boot' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
@@ -54,10 +54,10 @@ if ( post_password_required() ) {
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php _e( 'Comment navigation', 'under-boot' ); ?></h2>
-			<div class="nav-links">
+			<div class="nav-links pager">
 
-				<div class="nav-previous"><?php previous_comments_link( __( 'Older Comments', 'under-boot' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments', 'under-boot' ) ); ?></div>
+				<div class="nav-previous previous"><?php previous_comments_link( __( 'Older Comments', 'under-boot' ) ); ?></div>
+				<div class="nav-next next"><?php next_comments_link( __( 'Newer Comments', 'under-boot' ) ); ?></div>
 
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
