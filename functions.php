0b9d09d231bbe7bbe683cd0c2d79ba3c2911e34b
Doug Green <doug@dsgreen.com>
2015-06-01 20:53:58 -0400

    Add Bootstrap assets, jQuery, Modernizr, and examples

--- functions.php
+++ functions.php
@@ -120,13 +120,38 @@ add_action( 'widgets_init', 'under_boot_widgets_init' );
  * Enqueue scripts and styles.
  */
 function under_boot_scripts() {
-	wp_enqueue_style( 'under-boot-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'under-boot-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
+    // Bootstrap from CDN
+    wp_enqueue_style( 'bootstrap-3.3.4', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css', array(), '3.3.4' );
+    // various plugin styles (example)
+    // could opt to put in header-{custom}.php files if not used on every page, or leave here if not too big a file load
+    // RoyalSlider/Magnific Popup: http://dimsemenov.com
+    /*
+    wp_enqueue_style( 'royalslider', get_template_directory_uri()    . '/js/royalslider/royalslider.css', array(), '9.5.7' );
+    wp_enqueue_style( 'rs-default', get_template_directory_uri()     . '/js/royalslider/skins/default/rs-default.css', array(), '9.5.7' );
+    wp_enqueue_style( 'rs-custom', get_template_directory_uri()     . '/js/royalslider/skins/yoursite/your-custom-skin.css', array(), '9.5.7' );
+    wp_enqueue_style( 'magnific-popup', get_template_directory_uri() . '/css/magnific-popup.css', array(), '1.0.0' );
+    */
+    // underscores/site styles (follows bootstrap & plugins in case any overrides in main site styles)
+	wp_enqueue_style( 'main-styles', get_stylesheet_uri() );
+    // Modernizr (SVG, media query, add CSS classes, Modernizr.testStyles() build)
+    wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.custom.07230.js', array(), '2.8.3' );
+    // footer scripts
+    // underscores scripts
+    /*
+    wp_enqueue_script( 'under-boot-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 	wp_enqueue_script( 'under-boot-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+    */
+    // not using jquery dependency, WordPress adds it to header by default, want it in the footer and also using CDN
+    // at least 1.9.1 needed: https://github.com/twbs/bootstrap/blob/v3.3.4/bower.json
+    wp_enqueue_script( 'jquery_js', '//code.jquery.com/jquery-1.11.3.min.js', array(), '1.11.3', TRUE );
+    // jQuery migrate plugin if needed
+    // wp_enqueue_script( 'jquery_js', '//code.jquery.com/jquery-migrate-1.2.1.min.js', array(), '1.2.1', TRUE );
+    // Bootstrap JS from CDN
+    wp_enqueue_script( 'bootstrap_js', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array(), '3.3.4', TRUE );
+    // add plugin scripts here, followed by main site styles
+    wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/main.js', array(), '20150521', TRUE );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
