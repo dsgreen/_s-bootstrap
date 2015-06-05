2925fa54777ebbd191e6813c13bdf846cdd0208e
Doug Green <doug@dsgreen.com>
2015-06-05 13:56:57 -0400

    Add example code for ACF Pro

---
+++ examples/acf-general.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * ACF Pro examples
+ *
+ * Example code for a basic field type (text/textarea).
+ *
+ * @package UnderBoot
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="content-area container-fluid">
+        <main id="main" class="site-main" role="main">
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+            <div class="row">
+                <?php
+                $the_field = get_field('the_field') != '' ? get_field('the_field') : '';
+                ?>
+                <div class="col-sm-12">
+                    <?php echo $the_field; ?>
+                </div>
+            </div>
+
+            <?php endwhile; // End of the loop. ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
