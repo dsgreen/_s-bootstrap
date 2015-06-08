4bb2fde0bbeee91253c8ec7a35d235d84007beeb
Doug Green <doug@dsgreen.com>
2015-06-08 17:18:15 -0400

    Remove primary div and simplify classes

--- 404.php
+++ 404.php
@@ -7,50 +7,48 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area container-fluid">
-		<main id="main" class="site-main" role="main">
+                <main id="main" role="main" class="col-sm-9">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'under-boot' ); ?></h1>
-				</header><!-- .page-header -->
+                    <section class="error-404 not-found">
+                        <header class="page-header">
+                            <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'under-boot' ); ?></h1>
+                        </header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'under-boot' ); ?></p>
+                        <div class="page-content">
+                            <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'under-boot' ); ?></p>
 
-					<?php get_search_form(); ?>
+                            <?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+                            <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					<?php if ( under_boot_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'under-boot' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
+                            <?php if ( under_boot_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+                            <div class="widget widget_categories">
+                                <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'under-boot' ); ?></h2>
+                                <ul>
+                                <?php
+                                    wp_list_categories( array(
+                                        'orderby'    => 'count',
+                                        'order'      => 'DESC',
+                                        'show_count' => 1,
+                                        'title_li'   => '',
+                                        'number'     => 10,
+                                    ) );
+                                ?>
+                                </ul>
+                            </div><!-- .widget -->
+                            <?php endif; ?>
 
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'under-boot' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
+                            <?php
+                                /* translators: %1$s: smiley */
+                                $archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'under-boot' ), convert_smilies( ':)' ) ) . '</p>';
+                                the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+                            ?>
 
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+                            <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+                        </div><!-- .page-content -->
+                    </section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                </main>
 
 <?php get_footer(); ?>
