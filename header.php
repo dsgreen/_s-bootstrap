49324bd27adf58c679900ca1edca45323bdfda50
Doug Green <doug@dsgreen.com>
2015-06-08 17:14:43 -0400

    Add IE edge meta tag

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <!--[if !IE]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
