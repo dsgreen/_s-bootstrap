d1c5304555c4eed626ecaab00f262a5284b61313
Doug Green <doug@dsgreen.com>
2015-06-08 17:19:56 -0400

    Simplify classes, add class for two column layout

--- sidebar.php
+++ sidebar.php
@@ -10,6 +10,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+                <div id="secondary" role="complementary" class="col-sm-3">
+                    <?php dynamic_sidebar( 'sidebar-1' ); ?>
+                </div><!-- #secondary -->
