6d5618d422b86ae82c9643f525f608494f75b2f0
Doug Green <doug@dsgreen.com>
2015-06-08 17:19:21 -0400

    Remove primary div, simplify classes, add class for two column layout

--- single.php
+++ single.php
@@ -7,26 +7,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area container-fluid">
-		<main id="main" class="site-main" role="main">
+                <main id="main" class="site-main col-sm-8" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+                    <?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
+                    <?php the_post_navigation(); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+                    <?php
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
+                    ?>
 
-		<?php endwhile; // End of the loop. ?>
+                <?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                </main><!-- #main -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
